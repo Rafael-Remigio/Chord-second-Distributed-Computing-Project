c9670afb2432ddc134b548b068ce92b8c7e971d6
rafael <rafael.lixa@ua.pt>
2022-05-02 14:20:20 +0100

    Esqueci-me de dar merge

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """Tests two clients."""
 import pytest
 from utils import contains
@@ -18,24 +17,3 @@ def test_contains():
     assert contains(800, 300, 300)
     assert not contains(800, 300, 700)
     assert not contains(800, 300, 400)
-=======
-"""Tests two clients."""
-import pytest
-from utils import contains
-
-
-def test_contains():
-    # begin, end, node
-    assert contains(100, 300, 200)
-    assert contains(100, 300, 300)
-    assert not contains(100, 300, 100)
-    assert not contains(100, 300, 400)
-    assert not contains(100, 300, 0)
-    assert not contains(100, 300, 900)
-
-    assert contains(800, 300, 900)
-    assert contains(800, 300, 200)
-    assert contains(800, 300, 300)
-    assert not contains(800, 300, 700)
-    assert not contains(800, 300, 400)
->>>>>>> upstream/master
